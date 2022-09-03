fd5febd07d9bc9736a9be894ee084e014762cbe8
unknown <kaah6978@gmail.com>
2022-09-03 09:10:29 +0300

    make it better

--- routes/auth.js
+++ routes/auth.js
@@ -73,6 +73,38 @@ authRouter.get("/", auth, async (req, res) => {
     const user = await User.findById(req.user);
     res.json({ ...user._doc, token: req.token });
   });
+
+authRouter.put("/api/update/profile", auth, async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+    if (user) {
+        
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.phone = req.body.phone || user.phone
+        user.address = req.body.address || user.address
+        
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save() 
+        
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            address: updatedUser.address,
+            phone: updatedUser.phone,
+        })
+        
+    }
+    else {
+        res.status(404)
+        throw new Error ('User Not Found')
+    }
+
+  })
   
 
 module.exports = {authRouter}
