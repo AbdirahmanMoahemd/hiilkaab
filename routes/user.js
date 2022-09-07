d58480ae706653bf1c665f762a0769626771adc1
unknown <kaah6978@gmail.com>
2022-09-07 17:52:00 +0300

    make it better

--- routes/user.js
+++ routes/user.js
@@ -39,6 +39,42 @@ userRouter.post("/api/add-to-cart", auth, async (req, res) => {
 });
 
 
+userRouter.post("/api/remove-cartitem",auth, async (req, res) => {
+  try {
+    const { index } = req.body;
+    // const product = await Product.findById(id);
+    let user = await User.findById(req.user);
+
+    
+
+    if (user.cart.length == 0) {
+      user.cart.splice(index, 1);
+    } 
+    //else {
+    //   let isProductFound = false;
+    //   for (let i = 0; i < user.cart.length; i++) {
+    //     if (user.cart[i].product._id.equals(product._id)) {
+    //       isProductFound = true;
+    //     }
+    //   }
+
+    //   if (isProductFound) {
+    //     let producttt = user.cart.find((productt) =>
+    //       productt.product._id.equals(product._id)
+    //     );
+    //     producttt.quantity += 1;
+    //   } else {
+    //     user.cart.push({ product, quantity: 1,sizes, colors });
+    //   }
+    // }
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+
 
 
 userRouter.post("/api/add-to-cartMeal", auth, async (req, res) => {
