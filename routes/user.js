01d5c5fa925cffd6e91a563f0fe3a6736ab956e8
unknown <kaah6978@gmail.com>
2022-09-08 16:47:35 +0300

    make it better

--- routes/user.js
+++ routes/user.js
@@ -273,6 +273,65 @@ userRouter.post("/api/order", auth, async (req, res) => {
   }
 });
 
+
+
+userRouter.post("/api/order/cod", auth, async (req, res) => {
+  try {
+    const { cart,cartMeal,paymentMethod, shippingPrice,isPaid, totalPrice, address } = req.body;
+    let products = [];
+
+    for (let i = 0; i < cart.length; i++) {
+      let product = await Product.findById(cart[i].product._id);
+      if (product.quantity >= cart[i].quantity) {
+        product.quantity -= cart[i].quantity;
+        products.push({ product, quantity: cart[i].quantity, });
+        await product.save();
+      } else {
+        return res
+          .status(400)
+          .json({ msg: `${product.name} is out of stock!` });
+      }
+    }
+
+    let meals = [];
+
+    for (let i = 0; i < cartMeal.length; i++) {
+      let meal = await Meal.findById(cartMeal[i].meal._id);
+      if (meal.quantity >= cartMeal[i].quantity) {
+        meal.quantity -= cartMeal[i].quantity;
+        meals.push({ meal, quantity: cartMeal[i].quantity });
+        await meal.save();
+      } else {
+        return res
+          .status(400)
+          .json({ msg: `${meal.name} is out of stock!` });
+      }
+    }
+
+    let user = await User.findById(req.user);
+    user.cart = [];
+    user.cartMeal = [];
+    user = await user.save();
+
+    let order = new Order({
+      products,
+      meals,
+      shippingPrice,
+      paidAt: new Date(),
+      isPaid,
+      paymentMethod,
+      totalPrice,
+      address,
+      userId: req.user,
+      orderedAt: new Date().getTime(),
+    });
+    order = await order.save();
+    res.json(order);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 userRouter.get("/api/orders/me", auth, async (req, res) => {
   try {
     const orders = await Order.find({ userId: req.user });
