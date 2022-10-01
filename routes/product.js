f5f66dc7d3730b0e35a2efeae5154b51477aa7c1
unknown <kaah6978@gmail.com>
2022-10-01 17:02:15 +0300

    make it better

--- routes/product.js
+++ routes/product.js
@@ -40,7 +40,7 @@ productRouter.get("/api/products/out", async (req, res) => {
 productRouter.get("/api/products/category/:name", async (req, res) => {
   try {
     let products = await Product.find({category: { $regex: req.params.name },isFeatured:true});
-    products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+    
     res.json(products);
   } catch (e) {
     res.status(500).json({ error: e.message });
@@ -62,7 +62,7 @@ productRouter.get("/api/products/:id", async (req, res) => {
 productRouter.get("/api/top-products/", async (req, res) => {
   try {
     const products = await Product.find({}).sort({ rating: -1 }).limit(30);
-    products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+    
     res.json(products);
   } catch (e) {
     res.status(500).json({ error: e.message });
@@ -90,7 +90,7 @@ productRouter.get("/products/admin/deal_of_day", async (req, res) => {
     let products = await Product.find({ isDiscounted: true });
 
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     } else {
       products = [];
@@ -108,7 +108,7 @@ productRouter.get("/products/deal_of_day/:name", async (req, res) => {
     let products = await Product.find({ isDiscounted: true,category: { $regex: req.params.name },isFeatured:true },);
 
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     } 
   } catch (e) {
@@ -122,7 +122,7 @@ productRouter.get("/products/sub/deal_of_day/:name", async (req, res) => {
     let products = await Product.find({ isDiscounted: true,subcategory: { $regex: req.params.name },isFeatured:true },);
 
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     } 
   } catch (e) {
@@ -138,7 +138,7 @@ productRouter.get("/products/search/:name", auth, async (req, res) => {
       name: { $regex: req.params.name, $options: "i" },isFeatured:true
     });
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     }
   } catch (e) {
@@ -153,7 +153,6 @@ productRouter.get("/products/admin/search/:name", auth, async (req, res) => {
       name: { $regex: req.params.name, $options: "i" },
     });
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
       res.json(products);
     }
   } catch (e) {
@@ -196,7 +195,7 @@ productRouter.get("/products/hprice/:name", async (req, res) => {
     const products = await Product.find({category: { $regex: req.params.name },isFeatured:true}).sort({ price: -1 });
     
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     }
   } catch (e) {
@@ -209,7 +208,7 @@ productRouter.get("/products/sub/hprice/:name", async (req, res) => {
     const products = await Product.find({subcategory: { $regex: req.params.name },isFeatured:true}).sort({ price: -1 });
     
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     }
   } catch (e) {
@@ -222,7 +221,7 @@ productRouter.get("/products/lprice/:name", async (req, res) => {
     const products = await Product.find({category: { $regex: req.params.name},isFeatured:true}).sort({ price: 1 });
 
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     }
     
@@ -237,7 +236,7 @@ productRouter.get("/products/sub/lprice/:name", async (req, res) => {
     const products = await Product.find({subcategory: { $regex: req.params.name},isFeatured:true}).sort({ price: 1 });
     
     if (products) {
-      products.sort((a, b) => (a._id > b._id) ? -1 : 1)
+      
       res.json(products);
     }
     
